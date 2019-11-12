85c484602becdbdb9a8fc9f0a2edc95a8ee9f95f
Josh Castillo <ramonjmcastillo@gmail.com>
2019-11-12 12:42:21 +0800

    Added edit and delete icons, added line through if task is completed

--- src/TodoApp/TodoList/TodoList.jsx
+++ src/TodoApp/TodoList/TodoList.jsx
@@ -2,8 +2,10 @@ import React from "react";
 import Paper from "@material-ui/core/Paper";
 import List from "@material-ui/core/List";
 import ListItem from "@material-ui/core/ListItem";
-import Divider from "@material-ui/core/Divider";
 import ListItemText from "@material-ui/core/ListItemText";
+import Divider from "@material-ui/core/Divider";
+
+import Todo from "./Todo";
 
 const TodoList = props => {
   return (
@@ -12,9 +14,7 @@ const TodoList = props => {
         {props.todos.map(todo => {
           return (
             <>
-              <ListItem>
-                <ListItemText>{todo.task}</ListItemText>
-              </ListItem>
+              <Todo task={todo.task} key={todo.id} completed={todo.completed} />
               <Divider />
             </>
           );
